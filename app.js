bf7049ae6c34101f028c2569b3bac20be0ff6298
Kasun Randeni <kasun@creativesoftware.com>
2025-08-16 23:30:12 +0530

    pp

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-function sendToExtension() {
+
+function sendMessage(action, data) {
   const extensionId = "fhafdbecdohieooenndcbmgcdnabodin"; // Replace with your extension's ID
   if (typeof chrome !== "undefined" && chrome.runtime) {
-    chrome.runtime.sendMessage(extensionId, { message: "Hello from PWA" }, (response) => {
+    chrome.runtime.sendMessage(extensionId, { action, ...data }, (response) => {
       if (chrome.runtime.lastError) {
         console.error("Error sending message from PWA:", chrome.runtime.lastError.message);
         return;
@@ -11,4 +12,16 @@ function sendToExtension() {
   } else {
     console.error("Chrome runtime not available");
   }
+}
+
+function changeVolume() {
+  sendMessage("changeVolume", { value: 70 });
+}
+
+function switchOutput() {
+  sendMessage("switchOutput", { device: "Speakers" });
+}
+
+function printImage() {
+  sendMessage("printImage", { image: "test.png" });
 }
