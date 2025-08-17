4dea0b0bcc3bb9b1b667fc6c3c0fc14e11201afb
Kasun Randeni <kasun@creativesoftware.com>
2025-08-17 23:04:19 +0530

    111

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 
 function sendMessage(action, data) {
-  const extensionId = "fhafdbecdohieooenndcbmgcdnabodin"; // Replace with your extension's ID
-  if (typeof chrome !== "undefined" && chrome.runtime) {
-    chrome.runtime.sendMessage(extensionId, { action, ...data }, (response) => {
-      if (chrome.runtime.lastError) {
-        console.error("Error sending message from PWA:", chrome.runtime.lastError.message);
-        return;
-      }
-      console.log("Response from extension:", response);
-      //document.getElementById("result").innerText = JSON.stringify(response);
-    });
-  } else {
-    console.error("Chrome runtime not available");
-  }
+  // const extensionId = "fhafdbecdohieooenndcbmgcdnabodin"; // Replace with your extension's ID
+  // if (typeof chrome !== "undefined" && chrome.runtime) {
+  //   chrome.runtime.sendMessage(extensionId, { action, ...data }, (response) => {
+  //     if (chrome.runtime.lastError) {
+  //       console.error("Error sending message from PWA:", chrome.runtime.lastError.message);
+  //       return;
+  //     }
+  //     console.log("Response from extension:", response);
+  //     //document.getElementById("result").innerText = JSON.stringify(response);
+  //   });
+  // } else {
+  //   console.error("Chrome runtime not available");
+  // }
+
+  window.postMessage({ type: "PRINT", payload: { text: "Hello Printer!" } }, "*");
 }
+
+
+
+// Listen for response
+window.addEventListener("message", (event) => {
+  console.log("Printer response:", event.data.payload);
+  
+});
 
 const consoleDiv = document.getElementById("result");
 
